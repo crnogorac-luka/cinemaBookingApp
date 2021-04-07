17d74b60022a0fbe7e666d50945c3c658690ff5b
luxhs <lc4802@rit.edu>
2021-04-07 20:03:50 +0200

    DB host name changed

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DBConnect {
     private String userName;
@@ -49,7 +50,7 @@ public class DBConnect {
 
     public boolean connect(){
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+ mySQL,userName,password);
+            connection = DriverManager.getConnection("jdbc:mysql://freedb.tech:3306/"+ mySQL,userName,password);
             return true;
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -63,5 +64,136 @@ public class DBConnect {
             throwables.printStackTrace();
         }
         return true;
+    }
+
+
+
+
+
+    // CRUD OPERATIONS
+
+    //prepare
+    public PreparedStatement prepare(String query, ArrayList<String> values) {
+        PreparedStatement statement = null;
+        int placeholderCount = 0;
+        try {
+            if (connect()) {
+                statement = connection.prepareStatement(query);
+                String query2 = query;
+                placeholderCount = query2.length() - query2.replace("?", "").length();
+                for (int i=0; i<placeholderCount; i++) {
+                    statement.setString(i+1, values.get(i));
+                }
+            }
+        } catch (Exception e) {
+            //System.out.println("Error occurred...");
+            e.printStackTrace();
+        }
+
+        return statement;
+    }
+
+
+
+    // READ
+    public ArrayList<ArrayList<String>> getData(String query, ArrayList<String> values, boolean hasHeader) {
+
+        ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>();
+
+        try {
+            if (connect()) {
+                PreparedStatement stmt = prepare(query, values);
+                ResultSet rs = stmt.executeQuery();
+                ResultSetMetaData rsmd = rs.getMetaData();
+
+                if (hasHeader) {
+                    ArrayList<String> header = new ArrayList<String>();
+
+                    for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                        String columnName = rsmd.getColumnName(i);
+                        header.add(columnName);
+                    }
+                    table.add(header);
+                }
+
+                while (rs.next()) {
+                    ArrayList<String> currRow = new ArrayList<String>();
+                    for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                        currRow.add(rs.getString(i));
+                    }
+                    table.add(currRow);
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+
+        return table;
+    }
+
+    // CREATE, UPDATE, DELETE
+    public boolean setData(String query, ArrayList<String> values) {
+        try {
+            if (connect()) {
+                PreparedStatement stmt = prepare(query, values);
+                int numRes = stmt.executeUpdate();
+                if (numRes == 0) return false;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+
+        return true;
+
+    }
+
+
+
+
+
+    // TRANSACTIONS
+
+    public void startTrans() {
+        try {
+            if (connect()) {
+                connection.setAutoCommit(false);
+                connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+            }
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+
+    public void endTrans() {
+        try {
+            if (connection != null) {
+                connection.setAutoCommit(true);
+            }
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+
+
+    public void rollbackTrans() {
+        try {
+            if (connection != null) {
+                connection.rollback();
+            }
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            close();
+        }
     }
 }
