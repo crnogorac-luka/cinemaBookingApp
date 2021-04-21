c10fee1e2148158c368fa8c9fb5ff7011734c952
luxhs <lc4802@rit.edu>
2021-04-21 17:55:59 +0200

    HomeCashier JFormDesigner

---
+++ src/HomeCashier.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import javax.swing.*;
+import com.intellij.uiDesigner.core.*;
+/*
+ * Created by JFormDesigner on Wed Apr 21 17:55:14 CEST 2021
+ */
+
+
+
+/**
+ * @author Luka Crnogorac
+ */
+public class HomeCashier extends JFrame {
+    public HomeCashier() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Luka Crnogorac
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new GridLayoutManager(3, 3, new Insets(0, 0, 0, 0), -1, -1));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Luka Crnogorac
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
