9f732179a5f2698abf83025c75d4ecabba7955c8
lpavic511 <lp7982@rit.edu>
2021-04-22 21:52:35 +0200

    Created RegisterPage class and RegisterPage jfd file

---
+++ src/RegisterPage.java
@@ -0,0 +1,101 @@
+import java.awt.*;
+import javax.swing.*;
+import com.intellij.uiDesigner.core.*;
+/*
+ * Created by JFormDesigner on Thu Apr 22 21:47:45 CEST 2021
+ */
+
+
+
+/**
+ * @author unknown
+ */
+public class RegisterPage extends JFrame {
+    public RegisterPage() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+        label1 = new JLabel();
+        label2 = new JLabel();
+        label3 = new JLabel();
+        email = new JTextField();
+        label4 = new JLabel();
+        password = new JTextField();
+        registerButton = new JButton();
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new GridLayoutManager(7, 1, new Insets(0, 0, 0, 0), -1, -1));
+
+        //---- label1 ----
+        label1.setText("Cinema Booking App");
+        label1.setForeground(new Color(102, 204, 255));
+        label1.setFont(label1.getFont().deriveFont(label1.getFont().getStyle() | Font.BOLD, label1.getFont().getSize() + 15f));
+        contentPane.add(label1, new GridConstraints(0, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            null, null, null));
+
+        //---- label2 ----
+        label2.setText("Register");
+        label2.setFont(label2.getFont().deriveFont(label2.getFont().getSize() + 7f));
+        contentPane.add(label2, new GridConstraints(1, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            null, null, null));
+
+        //---- label3 ----
+        label3.setText("E-mail");
+        contentPane.add(label3, new GridConstraints(2, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            null, null, null));
+        contentPane.add(email, new GridConstraints(3, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            new Dimension(200, 20), null, null));
+
+        //---- label4 ----
+        label4.setText("Password");
+        contentPane.add(label4, new GridConstraints(4, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            null, null, null));
+        contentPane.add(password, new GridConstraints(5, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            new Dimension(200, 20), null, null));
+
+        //---- registerButton ----
+        registerButton.setText("Register");
+        registerButton.setBackground(new Color(102, 204, 255));
+        contentPane.add(registerButton, new GridConstraints(6, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            null, null, null));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+    private JTextField email;
+    private JLabel label4;
+    private JTextField password;
+    private JButton registerButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
